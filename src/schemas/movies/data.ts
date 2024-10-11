496394a697f56a2e8e00e9e11b85d8be39f027cb
Riccardo Mura <riccardo_mura@yahoo.com>
2024-10-11 14:42:27 +0200

    Improve movie schemas

--- src/schemas/movies/data.ts
+++ src/schemas/movies/data.ts
@@ -1,59 +1,70 @@
 import { type Static, Type } from '@sinclair/typebox';
 
-export const MovieSchema = Type.Object({
-  awards: Type.Optional(
-    Type.Object({
+const MovieMandatoryFieldsSchema = Type.Object({
+  title: Type.String(),
+  type: Type.String()
+});
+
+const MovieOptionalFieldsSchema = Type.Partial(
+  Type.Object({
+    awards: Type.Object({
       wins: Type.Number(),
       nominations: Type.Number(),
       text: Type.String()
-    })
-  ),
-  cast: Type.Optional(Type.Array(Type.String())),
-  countries: Type.Optional(Type.Array(Type.String())),
-  directors: Type.Optional(Type.Array(Type.String())),
-  fullplot: Type.Optional(Type.String()),
-  genres: Type.Optional(Type.Array(Type.String())),
-  imdb: Type.Partial(
-    Type.Object({
-      id: Type.String(),
-      rating: Type.Number(),
-      votes: Type.Number()
-    })
-  ),
-  languages: Type.Optional(Type.Array(Type.String())),
-  lastupdated: Type.String(),
-  metacritic: Type.Optional(Type.Number()),
-  num_mflix_comments: Type.Number(),
-  plot: Type.Optional(Type.String()),
-  poster: Type.Optional(Type.String()),
-  rated: Type.Optional(Type.String()),
-  released: Type.Optional(Type.String()),
-  runtime: Type.Optional(Type.Number()),
-  title: Type.String(),
-  tomatoes: Type.Optional(
-    Type.Object({
-      viewer: Type.Optional(
-        Type.Partial(
-          Object({
-            meter: Type.Number(),
-            numReviews: Type.Number(),
-            rating: Type.Number()
-          })
-        )
-      ),
-      lastUpdated: Type.String()
-    })
-  ),
-  type: Type.String(),
-  writers: Type.Optional(Type.Array(Type.String())),
-  year: Type.Any()
+    }),
+    cast: Type.Array(Type.String()),
+    countries: Type.Array(Type.String()),
+    directors: Type.Array(Type.String()),
+    fullplot: Type.String(),
+    genres: Type.Array(Type.String()),
+    imdb: Type.Partial(
+      Type.Object({
+        id: Type.String(),
+        rating: Type.Number(),
+        votes: Type.Number()
+      })
+    ),
+    languages: Type.Array(Type.String()),
+    lastupdated: Type.String(),
+    metacritic: Type.Number(),
+    num_mflix_comments: Type.Number(),
+    plot: Type.String(),
+    poster: Type.String(),
+    rated: Type.String(),
+    released: Type.String(),
+    runtime: Type.Number(),
+    tomatoes: Type.Partial(
+      Type.Object({
+        viewer: Type.Optional(
+          Type.Partial(
+            Object({
+              meter: Type.Number(),
+              numReviews: Type.Number(),
+              rating: Type.Number()
+            })
+          )
+        ),
+        lastUpdated: Type.String()
+      })
+    ),
+    writers: Type.Array(Type.String()),
+    year: Type.Any()
+  })
+);
+
+export const MovieIdSchema = Type.String({
+  description: 'The unique identifier of the movie'
+});
+
+export const MovieSchema = Type.Object({
+  ...MovieMandatoryFieldsSchema.properties,
+  ...MovieOptionalFieldsSchema.properties
 });
 
 export const MovieWithIdSchema = Type.Object({
   ...MovieSchema.properties,
-  id: Type.String()
+  ...{ id: MovieIdSchema }
 });
-
 export const MovieListSchema = Type.Array(MovieWithIdSchema);
 
 export type MovieSchemaType = Static<typeof MovieSchema>;
