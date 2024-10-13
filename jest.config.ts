53ca1ed499c69a999d48fe92b8650dd21fd1eced
Riccardo Mura <riccardo_mura@yahoo.com>
2024-10-13 10:42:36 +0200

    Enable coverage data collection

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,14 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   testEnvironment: 'node'
 };
