a1c1bec81298251d486b80a376586b6d6f46eaab
Riccardo Mura <riccardo_mura@yahoo.com>
2024-10-28 12:30:20 +0100

    Add more test for collection utils

--- src/test/utils/collection-utils.test.ts
+++ src/test/utils/collection-utils.test.ts
@@ -3,11 +3,33 @@ import { Type } from '@sinclair/typebox';
 
 describe('collectionUtils', () => {
   it('should correctly build the search object', () => {
-    const schema = Type.Object({ title: Type.String(), year: Type.String() });
-    const filter = { search: 'title:foo,year:bar' };
+    const schema = Type.Object({ foo: Type.String(), bar: Type.String() });
+    const filter = { search: 'foo:foo,bar:bar' };
     const search = getGenericSearch(schema, filter);
-    expect(search).toEqual({ title: /foo/i, year: /bar/i });
+    expect(search).toEqual({ foo: /foo/i, bar: /bar/i });
   });
+
+  it('should handle numeric search values', () => {
+    const schema = Type.Object({ foo: Type.Integer(), bar: Type.Integer() });
+    const filter = { search: 'foo:1,bar:2' };
+    const search = getGenericSearch(schema, filter);
+    expect(search).toEqual({ foo: 1, bar: 2 });
+  });
+
+  it('should throw an error for an invalid property key', () => {
+    const schema = Type.Object({ foo: Type.String(), bar: Type.String() });
+    const filter = { search: 'invalid:foo' };
+    expect(() => getGenericSearch(schema, filter)).toThrow();
+  });
+
+  it('should throw an error for an unsupported search type', () => {
+    const schema = Type.Object({
+      foo: Type.Object({ foo: Type.Void(), bar: Type.Void() })
+    });
+    const filter = { search: 'foo:foo' };
+    expect(() => getGenericSearch(schema, filter)).toThrow();
+  });
+
   it('should correctly build the sort object', () => {
     const filter = { sort: 'year:asc,title:desc' };
     const sort = getGenericSort(filter, {});
