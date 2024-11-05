5a972bcc22c8298d4a94b7d32772ffd87a0b41a4
Riccardo Mura <riccardo_mura@yahoo.com>
2024-11-05 10:03:58 +0100

    Add unit tests for caching

--- src/plugins/cache.ts
+++ src/plugins/cache.ts
@@ -26,94 +26,116 @@ const getMaxAge = (request: FastifyRequest): number =>
       AppConfigDefaults.cacheExpiration_s.toString()
   );
 
+const putInCache = async (
+  fastify: FastifyInstance,
+  request: FastifyRequest,
+  reply: FastifyReply,
+  payload: unknown
+): Promise<boolean> => {
+  if (!isCacheable(request, reply) || doesNotAllowCache(request)) {
+    fastify.log.info(
+      `Caching bypassed based on ${reply.statusCode}@${request.method}@${request.url}`
+    );
+    return false;
+  }
+
+  const cacheKey = CacheUtils.genCacheKey(request);
+
+  fastify.cache.get(cacheKey, (err, value) => {
+    if (err != null) {
+      fastify.log.error(err);
+      return false;
+    }
+    if (value != null) {
+      fastify.log.info('Cache hit, not storing response');
+      return false;
+    }
+
+    const headers = { ...reply.getHeaders() };
+    const cachedAt = Date.now();
+    const cacheValue = { headers, payload, cachedAt };
+
+    fastify.cache.set(cacheKey, cacheValue, AppConfigDefaults.cacheExpiration_s * 1000, (err) => {
+      if (err != null) {
+        fastify.log.error(err);
+      }
+    });
+
+    const cacheKeySignature = getKeySignature(cacheKey);
+    fastify.log.info(`Cached response for key: ${cacheKeySignature}`);
+  });
+
+  return true;
+};
+
+const getFromCache = async (
+  fastify: FastifyInstance,
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<boolean> => {
+  if (!isCacheable(request)) {
+    fastify.log.info(`Route ${request.method}@${request.url} is not cacheable`);
+    return false;
+  }
+
+  if (doesNotAllowCache(request)) {
+    fastify.log.info('Cache bypassed due to no-cache directive');
+    return false;
+  }
+
+  const cacheKey = CacheUtils.genCacheKey(request);
+
+  fastify.cache.get(cacheKey, (err, value) => {
+    if (err != null) {
+      fastify.log.error(err);
+
+      return false;
+    }
+
+    const cacheKeySignature = getKeySignature(cacheKey);
+
+    if (value != null) {
+      fastify.log.info(`Cache hit for key: ${cacheKeySignature}`);
+
+      const { payload, headers, cachedAt } = value.item as {
+        payload: string;
+        headers: Record<string, string>;
+        cachedAt: number;
+      };
+
+      const age = Math.ceil((Date.now() - cachedAt) / 1000);
+      const maxAge = getMaxAge(request);
+
+      if (age > maxAge) {
+        fastify.log.info(`Cache stale for key: ${cacheKeySignature}`);
+
+        return false;
+      }
+
+      reply.headers({ ...headers, age: age.toString() }).send(JSON.parse(payload));
+    } else {
+      fastify.log.info(`Cache miss for key: ${cacheKeySignature}`);
+      reply.header('last-modified', new Date().toUTCString());
+
+      return false;
+    }
+  });
+
+  return true;
+};
+
 const cachePlugin = fp(
   async (fastify: FastifyInstance) => {
     fastify.addHook('onRequest', async (request, reply) => {
-      if (!isCacheable(request)) {
-        fastify.log.info(`Route ${request.method}@${request.url} is not cacheable`);
-        return;
-      }
-
-      if (doesNotAllowCache(request)) {
-        fastify.log.info('Cache bypassed due to no-cache directive');
-        return;
-      }
-
-      const cacheKey = CacheUtils.genCacheKey(request);
-
-      fastify.cache.get(cacheKey, (err, value) => {
-        if (err != null) {
-          fastify.log.error(err);
-          return;
-        }
-
-        const cacheKeySignature = getKeySignature(cacheKey);
-
-        if (value != null) {
-          fastify.log.info(`Cache hit for key: ${cacheKeySignature}`);
-
-          const { payload, headers, cachedAt } = value.item as {
-            payload: string;
-            headers: Record<string, string>;
-            cachedAt: number;
-          };
-
-          const age = Math.ceil((Date.now() - cachedAt) / 1000);
-          const maxAge = getMaxAge(request);
-
-          if (age > maxAge) {
-            fastify.log.info(`Cache stale for key: ${cacheKeySignature}`);
-            return;
-          }
-          reply.headers({ ...headers, age: age.toString() }).send(JSON.parse(payload));
-        } else {
-          fastify.log.info(`Cache miss for key: ${cacheKeySignature}`);
-          reply.header('last-modified', new Date().toUTCString());
-        }
-      });
+      await getFromCache(fastify, request, reply);
     });
 
     fastify.addHook('onSend', async (request, reply, payload) => {
-      if (!isCacheable(request, reply) || doesNotAllowCache(request)) {
-        fastify.log.info(
-          `Caching bypassed based on ${reply.statusCode}@${request.method}@${request.url}`
-        );
-        return;
-      }
-
-      const cacheKey = CacheUtils.genCacheKey(request);
-
-      fastify.cache.get(cacheKey, (err, value) => {
-        if (err != null) {
-          fastify.log.error(err);
-          return;
-        }
-        if (value != null) {
-          fastify.log.info('Cache hit, not storing response');
-          return;
-        }
-
-        const headers = { ...reply.getHeaders() };
-        const cachedAt = Date.now();
-        const cacheValue = { headers, payload, cachedAt };
-
-        fastify.cache.set(
-          cacheKey,
-          cacheValue,
-          AppConfigDefaults.cacheExpiration_s * 1000,
-          (err) => {
-            if (err != null) {
-              fastify.log.error(err);
-            }
-          }
-        );
-
-        const cacheKeySignature = getKeySignature(cacheKey);
-        fastify.log.info(`Cached response for key: ${cacheKeySignature}`);
-      });
+      await putInCache(fastify, request, reply, payload);
     });
   },
   { name: 'cache', dependencies: ['server-config'] }
 );
 
 export default cachePlugin;
+export { doesNotAllowCache, getFromCache, getMaxAge, isCacheable, putInCache };
